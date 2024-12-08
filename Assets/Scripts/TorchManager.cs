dfa0ebc996aaa9e25aa46ec9935a40430fa7ce97
Msucharz <maciej.max.sucharzewski@gmail.com>
2024-12-08 21:06:30 +0100

    added flicker to torch lights
    added changing intensity controlled via script to torches, also fixed a mistake made in previous commit (particle system on wrong object)

--- Assets/Scripts/TorchManager.cs
+++ Assets/Scripts/TorchManager.cs
@@ -6,14 +6,18 @@ public class TorchManager : MonoBehaviour
 {
     [SerializeField] private bool isTorchOn = false;
     [SerializeField] private Light TorchLight;
+    [SerializeField] private ParticleSystem fire_particles;
 
     private void Update()
     {
+        fire_particles = GetComponent<ParticleSystem>();
         LightingTorch();
     }
 
     private void LightingTorch()
     {
         TorchLight.enabled = isTorchOn;
+        var ps_emission = fire_particles.emission;
+        ps_emission.enabled = isTorchOn;
     }
 }
---
+++ Assets/Scripts/light_variable_intensity.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light_variable_intensity : MonoBehaviour
+{
+    public Light light;
+    public int seed;
+    public float start_intensity;
+    private float curr_intensity;
+    public float change_rate;
+    public float min_intensity;
+    public float max_intensity;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        curr_intensity = start_intensity;
+        Random.seed = seed;
+    }
+
+    void Update()
+    {
+        curr_intensity += Random.Range(change_rate*(-1),change_rate);
+        if (curr_intensity < min_intensity)
+        {
+            curr_intensity = min_intensity;
+        }
+        else if (curr_intensity > max_intensity)
+        {
+            curr_intensity = max_intensity;
+        }
+        light.intensity = curr_intensity;
+    }
+}
